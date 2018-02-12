e826e0435be2ee99d4c79ae2b3404ef08c31a642
JOSHYJOHNS <35687903+JOSHYJOHNS@users.noreply.github.com>
2018-02-12 13:11:33 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+   printf("Enter a string: ");
+   gets(str);
+   printf("Enter a character to find the frequency: ");
+   scanf("%c",&ch);
+   for(i = 0; str[i] != '\0'; ++i;
+   printf("Frequency of %c = %d", ch, frequency);
+   return 0;
+}
